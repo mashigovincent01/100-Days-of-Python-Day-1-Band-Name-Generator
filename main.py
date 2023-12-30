694d8170e86549a55da70716b71e5ae778cf9522
mashigovincent01 <mashigovincent407@gmail.com>
2023-12-30 22:35:16 +0200

    final project

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+name = input("What is the name of the city  you grew up in? ")
+petName = input("What is the name of your pet? ")
+
+print(f"Your brand name is : {name} {petName}")
